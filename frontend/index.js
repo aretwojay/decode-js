83bc3b6ef1c63778d77841b6f2a871c1e57d23ee
Iris Ayivodji <iris06pro@gmail.com>
2026-07-08 16:01:38 +0200

    feat(theme): implémenter un système de thèmes multiples
    Ajoute un chargeur de thème (theme.js) basé sur createState : setTheme()
    change dynamiquement la feuille de style chargée (balise <link>), sans
    toucher au moteur (routeur, rendu, state). Trois thèmes de démonstration
    en attendant les designs Figma définitifs.
    
    Closes #33

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,7 @@
 import BrowserRouter from "./components/router/browser-router.js";
 import routes from "./routes/index.js";
 import "./lib/string-interpolate.js";
-
+import "./lib/theme.js";
 const rootElement = document.getElementById("root");
 BrowserRouter(rootElement, routes);
 
