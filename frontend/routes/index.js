6963dcb2d975038581d3f7f96e447f00b05f0989
yascodev <yaniss.lambeau@gmail.com>
2026-07-22 09:26:59 +0200

    feat(infra): dockeriser Strapi + nginx, configurer auth/rôles Strapi, fix routing SPA

--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -1,8 +1,10 @@
 import PageGallery from "../pages/gallery-page.js";
 import PageTable from "../pages/table-page.js";
+import PageExperience from "../pages/experience-page.js";
 import Page404 from "../pages/not-found-page.js";
 
 export default {
+  "/": PageExperience,
   "/table": PageTable,
   "/gallery": PageGallery,
   "/experiences": PageExperience,
